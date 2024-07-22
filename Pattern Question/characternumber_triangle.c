b4dafe7b54a3c849523f4c4db9063ce74a11558d
RutwikPatil2024 <rutwikpatil8000@gmail.com>
2024-07-22 16:21:40 +0530

    Character and Number Triangle

---
+++ Pattern Question/characternumber_triangle.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n ;
+    printf("enter the number n=");
+    scanf("%d",& n);
+    for(int i=1;i<=n;i++)
+    {
+        int a=1;
+        for(int j=1;j<=i;j++)
+        {
+            int d=a+64;
+            char ch=(char)d;
+           // printf(" %c ",ch);
+           if(i%2==0)
+           {
+            printf(" %c ",ch);
+           }
+           else
+           {
+            printf(" %d ",j);
+            
+
+           }
+            a++;
+            
+        }
+        printf("\n");
+    }
+}
