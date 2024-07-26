9bbca00d08f23f087aeddc071be12db543c5d6e6
RutwikPatil2024 <rutwikpatil8000@gmail.com>
2024-07-26 18:42:10 +0530

    Increment and Decrement

---
+++ loops/incre.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    // while (i++<10)
+    // {
+    //     printf("%d\n",i);
+    // }
+    while (++i<10)
+    {
+        printf("%d\n",i);
+    }
+    printf("Loop Ended\n");
+    return 0;
+}
